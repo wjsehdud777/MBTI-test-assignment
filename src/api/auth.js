6ca919df00f44aed6d4a3f7c5d92faade15ebfd1
JDY <wjsehdud@nate.com>
2025-02-24 21:26:13 +0900

    Project: 인증 API를 위한 axios 기반의 사용자 등록, 로그인, 프로필 조회 및 업데이트 기능 추가

---
+++ src/api/auth.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API_URL = "https://www.nbcamp-react-auth.link";
+
+export const register = async (userData) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+};
+
+export const login = async (userData) => {
+  const response = await axios.post(`${API_URL}/login`, userData);
+  return response.data;
+};
+
+export const getUserProfile = async (token) => {
+  const response = await axios.get(`${API_URL}/profile`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+export const updateProfile = async (formData, token) => {
+  const response = await axios.put(`${API_URL}/profile`, formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
