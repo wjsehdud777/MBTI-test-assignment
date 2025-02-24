2d74567ef991ce5dff1e6be333d83a2daa485dff
JDY <wjsehdud@nate.com>
2025-02-24 15:06:53 +0900

    Project: 회원가입 페이지 레이아웃 및 스타일 추가, 로그인 링크 연결

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-50">
+      <div className="bg-white p-8 rounded-lg shadow-xl w-full max-w-sm">
+        <button className="w-full py-3 bg-red-400 text-white font-semibold rounded-lg hover:bg-red-600 transition duration-400">
+          회원가입
+        </button>
+        <div className="mt-4 text-center">
+          <p className="text-gray-600">
+            이미 계정이 있으신가요?{" "}
+            <Link
+              to="/login"
+              className="text-red-700 hover:text-red-500 font-semibold"
+            >
+              로그인
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
