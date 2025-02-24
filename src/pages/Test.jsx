03684c002178ceb11a84777c4f46517bf7467a14
JDY <wjsehdud@nate.com>
2025-02-24 21:18:41 +0900

    Project: 테스트 결과 API 및 MBTI 테스트 페이지 구현

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,9 +1,51 @@
-const Test = () => {
+import { useState } from "react";
+import TestForm from "../components/TestForm";
+import { calculateMBTI, mbtiDescriptions } from "../utils/mbtiCalculator";
+import { createTestResult } from "../api/testResults";
+import { useNavigate } from "react-router-dom";
+
+const TestPage = ({ user }) => {
+  const navigate = useNavigate();
+  const [result, setResult] = useState(null);
+
+  const handleTestSubmit = async (answers) => {
+    const mbtiResult = calculateMBTI(answers);
+  };
+
+  const handleNavigateToResults = () => {
+    navigate("/results");
+  };
+
   return (
-    <div>
-      <h1>MBTI 테스트 페이지</h1>
+    <div className="w-full flex flex-col items-center justify-center bg-white">
+      <div className="bg-white rounded-lg p-8 max-w-lg w-full h-full overflow-y-auto">
+        {!result ? (
+          <>
+            <h1 className="text-3xl font-bold text-primary-color mb-6">
+              MBTI 테스트
+            </h1>
+            <TestForm onSubmit={handleTestSubmit} />
+          </>
+        ) : (
+          <>
+            <h1 className="text-3xl font-bold text-primary-color mb-6">
+              테스트 결과: {result}
+            </h1>
+            <p className="text-lg text-gray-700 mb-6">
+              {mbtiDescriptions[result] ||
+                "해당 성격 유형에 대한 설명이 없습니다."}
+            </p>
+            <button
+              onClick={handleNavigateToResults}
+              className="w-full bg-primary-color text-white py-3 rounded-lg font-semibold hover:bg-primary-dark transition duration-300 hover:text-[#FF5A5F]"
+            >
+              결과 페이지로 이동하기
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
 
-export default Test;
+export default TestPage;
